828022fb8f9977155f40caf352abc6423ebab79f
chaewon <cwlee4633@naver.com>
2023-07-26 23:48:36 +0900

    폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -49,7 +49,7 @@ public class ProductService {
     }
 
     //관심 상품 전체 조회
-    @Transactional
+    @Transactional(readOnly = true)
     public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
